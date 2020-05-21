d8c774318f70577e39f52d7fdcc0574bcac19541
Alex Garcia <g.nightster11@gmail.com>
2020-05-21 16:40:28 -0700

    got inviroment to work. made some small changes to page and re ran program with out problem.

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button tiitle="change text" />
+      <Text>you something to work!</Text>
     </View>
   );
 }
